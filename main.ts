93d586668c9392935d41a2e1e1ebd0d7c40c014c
gary91602000 <51686186+gary91602000@users.noreply.github.com>
2020-04-08 10:33:53 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -31,7 +31,7 @@ namespace test_pins {
     //% tpins.fieldOptions.columns=3
     //% inlineInputMode=inline
     export function digital_readpin(tpins: DigitalPin): number {
-        return pins.digitalReadPin(tpins: DigitalPin)
+        return pins.digitalReadPin(tpins)
     }
 	
 
